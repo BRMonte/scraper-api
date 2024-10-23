62e1bcd945e672820d7a918a7dc1e62527de03e2
Bruno Montebello <51917604+BRMonte@users.noreply.github.com>
2024-10-22 21:29:50 -0300

    Remove unnecessary require from controller

--- app/controllers/api/v1/scrapes_controller.rb
+++ app/controllers/api/v1/scrapes_controller.rb
@@ -1,6 +1,3 @@
-require_relative "../../../services/scraper_service"
-require_relative "../../../serializers/scrape_serializer"
-
 module Api
   module V1
     class ScrapesController < ApplicationController
