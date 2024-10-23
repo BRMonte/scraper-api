51cab98be0f995c0568e1970f67908808442403b
Bruno Montebello <51917604+BRMonte@users.noreply.github.com>
2024-10-22 21:27:19 -0300

    Refact controller

--- app/controllers/api/v1/scrapes_controller.rb
+++ app/controllers/api/v1/scrapes_controller.rb
@@ -1,8 +1,32 @@
+require_relative "../../../services/scraper_service"
+require_relative "../../../serializers/scrape_serializer"
+
 module Api
   module V1
     class ScrapesController < ApplicationController
       def index
-        render json: { data: "Hello, World!" }
+        url = params[:url]
+        fields = params[:fields] || {}
+
+        fields = normalize_fields(fields)
+
+        scraper_service = ScraperService.new(url, fields)
+        extracted_data = scraper_service.call
+
+        serializer = ScrapeSerializer.new(extracted_data, fields)
+        render json: serializer.serialize, status: :ok
+      end
+
+      private
+
+      def scrape_params
+        params.permit(:url, fields: {})
+      end
+
+      def normalize_fields(fields)
+        return fields.to_unsafe_h if fields.is_a?(ActionController::Parameters)
+
+        fields
       end
     end
   end
